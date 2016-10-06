95251ebad80f9e259309d7b08a88214822e35a95
kaitsu1 <kairi.papstel@gmail.com>
2016-10-06 23:51:13 +0300

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World!");
         System.out.println("Julge pealehakkamine on pool voitu!");
+        System.out.println("Git Pull");
     }
 }
