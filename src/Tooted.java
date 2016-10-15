36091a39ff0c365ed607ff110fcb7f98d3ab259f
kaitsu1 <kairi.papstel@gmail.com>
2016-10-16 02:02:15 +0300

    tooted

---
+++ src/Tooted.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * Created by kaits on 15.10.2016.
+ */
+public class Tooted {
+    public static void main(String[] args) {
+        Scanner klaviatuur = new Scanner(System.in);
+        System.out.println("Vali toode ja vajuta klahvi");
+        String toode = sc.nextLine();
+        System.out.println("Sisesta kogus-mitu tk?");
+        int kogus = sc.nextInt();
+
+        System.out.println("Sisesta enda sünniaasta:");
+        int synniaasta = klaviatuur.nextInt();
+    String toode1 = "moskva sai";
+    String toode2 = "kohupiima korp";
+    String toode3 = "õunakook";
+    String toode4 = "kirsistruudel";
+    String toode5 = "kaneelisai";
+    String toode6 = "sefiirikorv";
+    String toode7 = "muffin";
+    String toode8 = "aleksandrikook";
+    String toode9 = "vahukoorekook";
+    String toode10 = "kapsapirukas";
+    String toode11 = "porgandipirukas";
+    String toode12 = "lihapirukas";
+    String toode13 = "viineripirukas";
+    String toode14 = "minipitsa";
+    String toode15 = "ekleer";
+
+
+
+    }
+
+
+}
